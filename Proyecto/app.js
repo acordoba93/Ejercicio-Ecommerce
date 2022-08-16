8c5ee1be45c48e94bf8c7a5cc1f4828c4e48d1fc
Andres Felipe Cordoba Portilla <acordoba38@summa-sci.com>
2022-08-16 18:58:53 -0500

    Vista home y detalle del producto

--- Proyecto/app.js
+++ Proyecto/app.js
@@ -7,13 +7,21 @@ app.listen(process.env.PORT || 3000, function(){
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(__dirname + '/views/Home.html');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(__dirname + '/views/Login.html');
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(__dirname + '/views/Register.html');
+});
+
+app.get('/shoppingcart', (req,res)=>{
+    res.sendFile(__dirname + '/views/ShoppingCart.html');
+});
+
+app.get('/detail', (req,res)=>{
+    res.sendFile(__dirname + '/views/ProductDetail.html');
 });
