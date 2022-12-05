c8aa69138f3e8aeb3533fd4fce73e00709ba9699
lucasgarcia2022 <lucasgarciacab@hotmail.com>
2022-12-05 20:21:50 -0300

    validaciones works

--- public/js/formCrearProducto.js
+++ public/js/formCrearProducto.js
@@ -1,125 +1,93 @@
 window.addEventListener("load", () => {
 
-    let formCreate = document.querySelector(".form-create");
-    formCreate.productName.focus();
+    let formCrearProducto = document.querySelector(".formCrearProducto");
+    formCrearProducto.nombre.focus();
 
-    formCreate.addEventListener("submit", (event) => {
+    formCrearProducto.addEventListener("submit", (event) => {
 
 //Creamos un array de errores vacios para ir agregando los errores
 
      let errors =[];
      
-     let productNombre = document.querySelector("#productName");
-     let productDescripcion = document.querySelector("#productDescription");
-     let productTalle = document.querySelector("#productCategory");
-     let productPrecio = document.querySelector("#productPrice");
-     let productCategoria = document.querySelector("#productPrice");
-     let productImagen = document.querySelector("#productImage");
+     let nombre = document.querySelector("#nombre");
+     let descripcion = document.querySelector("#descripcion");
+     let talle = document.querySelector("#talle");
+     let precio = document.querySelector("#precio");
+     let categoria = document.querySelector("#categoria");
+     let imagen = document.querySelector("#imagen");
 
-     //-------productName-----------
-     if(productName.value == ""){
+     //-------nombre-----------
+     if(nombre.value.trim == ""){
         errors.push("El campo Nombre del Producto no puede estar vacío");
-        productName.classList.remove("is-valid");
-        productName.classList.add("is-invalid");
+        nombre.classList.remove("is-valid");
+        nombre.classList.add("is-invalid");
      }else if(productName.value.length < 5) {
         errors.push("El campo Nombre del Producto debe tener al menos 5 caracteres");
-        productName.classList.remove("is-valid");
-        productName.classList.add("is-invalid");
+        nombre.classList.remove("is-valid");
+        nombre.classList.add("is-invalid");
      }else {
-        productName.classList.add("is-valid");
-        productName.classList.remove("is-invalid");
-        formCreate.productDescription.focus()
+      nombre.classList.add("is-valid");
+      nombre.classList.remove("is-invalid");
+      formCrearProducto.descripcion.focus()
      }
 
-     //-------productDescription-----------
-     if(productDescription.value == ""){
+     //-------descripcion-----------
+     if(descripcion.value == ""){
       errors.push("El campo Descripción no puede estar vacío");
-      productDescription.classList.remove("is-valid");
-      productDescription.classList.add("is-invalid");
-     }else if(productDescription.value.length < 20) {
+      descripcion.classList.remove("is-valid");
+      descripcion.classList.add("is-invalid");
+     }else if(descripcion.value.length < 20) {
       errors.push("El campo Descripción debe tener al menos 20 caracteres");
-      productDescription.classList.remove("is-valid");
-      productDescription.classList.add("is-invalid");
+      descripcion.classList.remove("is-valid");
+      descripcion.classList.add("is-invalid");
      }else {
-      productDescription.classList.add("is-valid");
-      productDescription.classList.remove("is-invalid");
-      formCreate.productCategory.focus()
+      descripcion.classList.add("is-valid");
+      descripcion.classList.remove("is-invalid");
+      formCrearProducto.talle.focus()
      }
 
-     //-------productCategory-----------
-     if(productCategory.value == ""){
-        errors.push("El campo Categoría no puede estar vacío");
-        productCategory.classList.remove("is-valid");
-        productCategory.classList.add("is-invalid");
+     //-------talle-----------
+     if(talle.value == ""){
+        errors.push("El campo Talle del Producto no puede estar vacío");
+        talle.classList.remove("is-valid");
+        talle.classList.add("is-invalid");
        }else {
-        productCategory.classList.add("is-valid");
-        productCategory.classList.remove("is-invalid");
-        formCreate.productCondition.focus()
+         talle.classList.add("is-valid");
+         talle.classList.remove("is-invalid");
+         formCrearProducto.precio.focus()
        }   
 
-       //-------productConditionSell-----------
-       if(productConditionSell.value == ""){
-        errors.push("El campo Condición de Venta no puede estar vacío");
-        productConditionSell.classList.remove("is-valid");
-        productConditionSell.classList.add("is-invalid");
+       //-------precio-----------
+       if(precio.value == ""){
+        errors.push("El campo Precio del Producto no puede estar vacío");
+        precio.classList.remove("is-valid");
+        precio.classList.add("is-invalid");
        }else {
-        productConditionSell.classList.add("is-valid");
-        productConditionSell.classList.remove("is-invalid");
-        formCreate.productSize.focus()
+         precio.classList.add("is-valid");
+         precio.classList.remove("is-invalid");
+         formCrearProducto.categoria.focus()
        }   
 
-        //-------productSize-----------
-        if(productSize.value == ""){
-            errors.push("El campo Talle del Producto no puede estar vacío");
-            productSize.classList.remove("is-valid");
-            productSize.classList.add("is-invalid");
+        //-------categoria-----------
+        if(categoria.value == ""){
+            errors.push("El campo Categoria del Producto no puede estar vacío");
+            categoria.classList.remove("is-valid");
+            categoria.classList.add("is-invalid");
            }else {
-            productSize.classList.add("is-valid");
-            productSize.classList.remove("is-invalid");
-            formCreate.productStatus.focus()
+            categoria.classList.add("is-valid");
+            categoria.classList.remove("is-invalid");
+            formCrearProducto.imagen.focus()
            }      
 
-               //-------productStatus-----------
-          if(productStatus.value == ""){
-            errors.push("El campo Estado del Producto no puede estar vacío");
-            productStatus.classList.remove("is-valid");
-            productStatus.classList.add("is-invalid");
-           }else {
-            productStatus.classList.add("is-valid");
-            productStatus.classList.remove("is-invalid");
-            formCreate.productCondition.focus()}
-
-              //-------productCondition-----------
-          if(productCondition.value == ""){
-            errors.push("El campo Condición del Producto no puede estar vacío");
-            productCondition.classList.remove("is-valid");
-            productCondition.classList.add("is-invalid");
-           }else {
-            productCondition.classList.add("is-valid");
-            productCondition.classList.remove("is-invalid");
-            formCreate.productPrice.focus()
-           }
-
-             //-------productPrice-----------
-          if(productPrice.value == ""){
-            errors.push("El campo Precio del Producto no puede estar vacío");
-            productPrice.classList.remove("is-valid");
-            productPrice.classList.add("is-invalid");
-           }else {
-            productPrice.classList.add("is-valid");
-            productPrice.classList.remove("is-invalid");
-            formCreate.productImage.focus()
-           } 
-
-            //-------productImage-----------
-          if(productImage.value == ""){
+               //-------imagen-----------
+          if(imagen.value == ""){
             errors.push("El campo Imagen del Producto no puede estar vacío");
-            productImage.classList.remove("is-valid");
-            productImage.classList.add("is-invalid");
+            imagen.classList.remove("is-valid");
+            imagen.classList.add("is-invalid");
            }else {
-            productImage.classList.add("is-valid");
-            productImage.classList.remove("is-invalid");
-           } 
+            imagen.classList.add("is-valid");
+            imagen.classList.remove("is-invalid");
+            }
 
             
    //------Controlamos si hay errores------
@@ -135,7 +103,7 @@ window.addEventListener("load", () => {
          ulErrors.innerHTML += "<li>" + errors[i] + "</li>"; //Agregamos los errores en una lista
       }
    }else{
-      formCreate.submit();
+      formCrearProducto.submit();
    }
  })   
 })
--- public/js/register.js
+++ public/js/register.js
@@ -1,121 +1,117 @@
 window.addEventListener("load", () => {
 
-    let formRegistro = document.querySelector(".form-registro");
-    formRegistro.firstName.focus();
+    let formRegister = document.querySelector(".formRegister");
+    formRegister.nombre.focus();
 
-    formRegistro.addEventListener("submit", (event) => {
+    formRegister.addEventListener("submit", (event) => {
 
 //Creamos un array de errores vacios para ir agregando los errores
 
      let errors =[];
      
-     let firstName = document.querySelector("#firstName");
-     let lastName = document.querySelector("#lastName");
-     let email = document.querySelector("#email");
-     let address = document.querySelector("#address");
-     let nationality = document.querySelector("#nationality");
-     let image = document.querySelector("#image");
-     let password = document.querySelector("#password");
+     let nombre = document.querySelector("#nombre");
+     let pais = document.querySelector("#pais");
+     let celular = document.querySelector("#celular");
+     let usuario = document.querySelector("#usuario");
+     let contrasena = document.querySelector("#contrasena");
+     let repetir = document.querySelector("#repetir");
+     let foto = document.querySelector("#foto");
     
-     //-------firstName-----------
-     if(firstName.value == ""){
+     //-------nombre-----------
+     if(nombre.value == ""){
         errors.push("El campo Nombre no puede estar vacío");
-        firstName.classList.remove("is-valid");
-        firstName.classList.add("is-invalid");
-     }else if(firstName.value.length < 2) {
+        nombre.classList.remove("is-valid");
+        nombre.classList.add("is-invalid");
+     }else if(nombre.value.length < 2) {
         errors.push("El campo Nombre debe tener al menos 2 caracteres");
-        firstName.classList.remove("is-valid");
-        firstName.classList.add("is-invalid");
+        nombre.classList.remove("is-valid");
+        nombre.classList.add("is-invalid");
      }else {
-        firstName.classList.add("is-valid");
-        firstName.classList.remove("is-invalid");
-        formRegistro.lastName.focus()
+      nombre.classList.add("is-valid");
+      nombre.classList.remove("is-invalid");
+      formRegister.pais.focus()
      }
 
-     //-------lastName-----------
-     if(lastName.value == ""){
-      errors.push("El campo Apellido no puede estar vacío");
-      lastName.classList.remove("is-valid");
-      lastName.classList.add("is-invalid");
-     }else if(lastName.value.length < 2) {
-      errors.push("El campo Apellido debe tener al menos 2 caracteres");
-      lastName.classList.remove("is-valid");
-      lastName.classList.add("is-invalid");
+     //-------pais-----------
+     if(pais.value == ""){
+      errors.push("El campo Pais de Nacimiento no puede estar vacío");
+      pais.classList.remove("is-valid");
+      pais.classList.add("is-invalid");
      }else {
-      lastName.classList.add("is-valid");
-      lastName.classList.remove("is-invalid");
-      formRegistro.email.focus()
+      pais.classList.add("is-valid");
+      pais.classList.remove("is-invalid");
+      formRegister.usuario.focus()
      }
 
-    //-------Email-----------
-    let regEmail = /\S+@\S+\.+\S+/;
-    if(email.value == ""){
-      errors.push("El campo Email no puede estar vacío");
-      email.classList.remove("is-valid");
-      email.classList.add("is-invalid");
-    } else if(!regEmail.test(email.value)){          //.test "testea que ese email contenga @ y ."
+    //-------usuario-----------
+    let reglaUsuario = /\S+@\S+\.+\S+/;
+    if(usuario.value == ""){
+      errors.push("El campo Usuario no puede estar vacío");
+      usuario.classList.remove("is-valid");
+      usuario.classList.add("is-invalid");
+    } else if(!usuario.test(usuario.value)){          //.test "testea que ese email contenga @ y ."
       errors.push("Debe ingresar un email válido");
-      email.classList.add("is-invalid");
-      email.classList.remove("is-valid");     
+      usuario.classList.add("is-invalid");
+      usuario.classList.remove("is-valid");     
    }else {
-      email.classList.add("is-valid");
-      email.classList.remove("is-invalid");
-      formRegistro.address.focus()
+      usuario.classList.add("is-valid");
+      usuario.classList.remove("is-invalid");
+      formRegister.celular.focus()
    }
 
-    //-------Address-----------
-    if(address.value == ""){
-      errors.push("El campo Domicilio no puede estar vacío");
-      address.classList.remove("is-valid");
-      address.classList.add("is-invalid");
-   }else if(address.value.length < 5) {
-      errors.push("El campo Domicilio debe tener al menos 5 caracteres");
-      address.classList.remove("is-valid");
-      address.classList.add("is-invalid");
+    //-------celular-----------
+    if(celular.value == ""){
+      errors.push("El campo Numero de Celular no puede estar vacío");
+      celular.classList.remove("is-valid");
+      celular.classList.add("is-invalid");
+   }else if(celular.value.length < 5) {
+      errors.push("El campo Numero de Celular debe tener al menos 5 caracteres");
+      celular.classList.remove("is-valid");
+      celular.classList.add("is-invalid");
    }else {
-      address.classList.add("is-valid");
-      address.classList.remove("is-invalid");
-      formRegistro.nationality.focus()
+      celular.classList.add("is-valid");
+      celular.classList.remove("is-invalid");
+      formRegister.contrasena.focus()
    }
 
-    //-------Nationality-----------
-    if(nationality.value == ""){
-      errors.push("El campo Nacionalidad no puede estar vacío");
-      nationality.classList.remove("is-valid");
-      nationality.classList.add("is-invalid");
-   }else if(nationality.value.length < 2) {
-      errors.push("El campo Nacionalidad debe tener al menos 2 caracteres");
-      nationality.classList.remove("is-valid");
-      nationality.classList.add("is-invalid");
+    //-------contrasena-----------
+    if(contrasena.value == ""){
+      errors.push("El campo contrasena no puede estar vacío");
+      contrasena.classList.remove("is-valid");
+      contrasena.classList.add("is-invalid");
+   }else if(contrasena.value.length < 2) {
+      errors.push("El campo contrasena debe tener al menos 2 caracteres");
+      contrasena.classList.remove("is-valid");
+      contrasena.classList.add("is-invalid");
    }else {
-      nationality.classList.add("is-valid");
-      nationality.classList.remove("is-invalid");
-      formRegistro.image.focus()
+      contrasena.classList.add("is-valid");
+      contrasena.classList.remove("is-invalid");
+      formRegister.foto.focus()
    }
 
-    //-------Nationality-----------
-    if(image.value == ""){
+    //-------foto-----------
+    if(foto.value == ""){
       errors.push("Debe seleccionar una imágen en formato JPG - PNG ó JPEG");
-      image.classList.remove("is-valid");
-      image.classList.add("is-invalid");
+      foto.classList.remove("is-valid");
+      foto.classList.add("is-invalid");
    }else {
-      image.classList.add("is-valid");
-      image.classList.remove("is-invalid");
-      formRegistro.password.focus()
+      foto.classList.add("is-valid");
+      foto.classList.remove("is-invalid");
+      formRegister.repetir.focus()
    }
 
-   //-------Password-----------
-   if(password.value == ""){
+   //-------repetir-----------
+   if(repetir.value == ""){
     errors.push("El campo Contraseña no puede estar vacío");
-    password.classList.remove("is-valid");
-    password.classList.add("is-invalid");
- }else if(password.value.length < 8) {
+    repetir.classList.remove("is-valid");
+    repetir.classList.add("is-invalid");
+ }else if(repetir.value.length < 8) {
     errors.push("El campo Contraseña debe tener al menos 8 caracteres");
-    password.classList.remove("is-valid");
-    password.classList.add("is-invalid");
+    repetir.classList.remove("is-valid");
+    repetir.classList.add("is-invalid");
  }else {
-    password.classList.add("is-valid");
-    password.classList.remove("is-invalid");
+   repetir.classList.add("is-valid");
+   repetir.classList.remove("is-invalid");
  }
 
 
@@ -137,7 +133,7 @@ window.addEventListener("load", () => {
          ulErrors.innerHTML += "<li>" + errors[i] + "</li>"; //Agregamos los errores en una lista
       }
    }else{
-      formRegistro.submit();
+      formRegister.submit();
    }
  })   
 })
